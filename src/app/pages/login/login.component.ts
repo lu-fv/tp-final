f899c8771787af6763b8830e59f4274826881e6e
Lucia <lu_mdp89@hotmail.com>
2025-10-12 18:02:45 -0300

    creacion de formulario login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatButtonModule, MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatButton,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private formbuilder = inject(FormBuilder);
+  loginForm = this.formbuilder.group({
+    user: ['', [Validators.required, Validators.max(5)]],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+    ],
+  });
+  constructor() {}
+  get user(){
+    return this.loginForm.get("user")
+  }
 
+  get password(){
+    return this.loginForm.get("password")
+  }
+  submit(): void {
+    console.log(this.loginForm)
+    if (this.loginForm.invalid)
+      return;
+  }
 }
